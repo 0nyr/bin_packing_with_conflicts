587bfb0523930ac100bc8e9a2622e69b47417725
Daniel Araujo <64176714+dcunha95@users.noreply.github.com>
2024-03-29 16:00:58 -0300

    Update script_all.jl

--- script_all.jl
+++ script_all.jl
@@ -3,6 +3,24 @@ using Logging
 include("bpc.jl")
 include("data.jl")
 
+# first run to compile
+J, w, E, W = read_file("BPWC_0_6_8.txt") 
+solution, z = solve_bpc(J, E, w, W, verbose=1, run_ffd=true, max_iter=10000)
+
+
+# instance log
+global LOG_IO = open("logs/$(file_name).txt", "w+")
+
+# read instance
+J, w, E, W = read_file("$(file_name).txt")
+
+
+# solve and get time elapsed
+println(LOG_IO, "J: $(J)\nw: $(w)\nW: $(W)\nE: $(E)")
+passed_time = @elapsed solution, z = solve_bpc(J, E, w, W, verbose=1, run_ffd=true, max_iter=10000)
+println(LOG_IO, "solution: $(solution)\nz: $(z)")
+
+
 # General log
 general_log_io = open("log.txt", "w+")
 logger = SimpleLogger(general_log_io)
