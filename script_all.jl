10a74680f85b285712b9ab20df1fa2eea00965d1
Daniel Araujo <64176714+dcunha95@users.noreply.github.com>
2024-03-29 17:32:41 -0300

    update

--- script_all.jl
+++ script_all.jl
@@ -18,29 +18,30 @@ global_logger(logger) # set as default logger
 csv_table = open("time.csv", "w+")
 
 # Files
-# file_name = "toy"
-# file_name = "toy2"
-file_name = "BPWC_0_6_8"
 
-# instance log
-global LOG_IO = open("logs/$(file_name).txt", "w+")
+folder_path = "test"
+instances = [joinpath(folder_path, i) for i in readdir(joinpath("instances", folder_path))]
 
-# read instance
-J, w, E, W = read_file("$(file_name).txt")
+for file_path in instances
 
-
-# solve and get time elapsed
-println(LOG_IO, "J: $(J)\nw: $(w)\nW: $(W)\nE: $(E)")
-passed_time = @elapsed solution, z = solve_bpc(J, E, w, W, verbose=0, run_ffd=true, max_iter=10000)
-println(LOG_IO, "solution: $(solution)\nz: $(z)")
-
-# register at general log
-@info z solution "$(passed_time) seconds"
-
-# save csv
-println(csv_table, "$(file_name), $(passed_time)")
-
-
+    # instance log
+    global LOG_IO = open(joinpath("logs/", file_path), "w+")
+    
+    # read instance
+    J, w, E, W = read_file(file_path)
+    
+    # solve and get time elapsed
+    println(LOG_IO, "J: $(J)\nw: $(w)\nW: $(W)\nE: $(E)")
+    passed_time = @elapsed solution, z = solve_bpc(J, E, w, W, verbose=0, run_ffd=true, max_iter=10000)
+    println(LOG_IO, "solution: $(solution)\nz: $(z)")
+    
+    # register at general log
+    @info z solution "$(passed_time) seconds"
+    
+    # save csv
+    instance_name = basename(file_path)[1:end-4]
+    println(csv_table, "$(instance_name), $(passed_time)")
+end
 
 # Close the file
 close(general_log_io)
